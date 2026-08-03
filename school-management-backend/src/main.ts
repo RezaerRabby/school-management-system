182746b8880df61070b433855ecf0a5b30dae28d
Rezae Rabby <rezaerrabby@gmail.com>
2026-08-03 22:59:16 +0600

    user auth done

--- school-management-backend/src/main.ts
+++ school-management-backend/src/main.ts
@@ -1,8 +1,32 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(5001);
+  app.setGlobalPrefix('api');
+
+  // Global Validation Pipe
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      transform: true,
+    }),
+  );
+
+  // Swagger
+  const config = new DocumentBuilder()
+    .setTitle('School Management API')
+    .setDescription('School Management System API Documentation')
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+  await app.listen(process.env.PORT ?? 5000);
 }
+
 bootstrap();
+
